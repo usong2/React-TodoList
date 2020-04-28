59f8eafc4a2787e6b043ccd3848a83e6481bd7fb
usong2 <dbthd6@naver.com>
2020-04-28 18:11:57 +0900

    function

--- src/components/TodoHead.js
+++ src/components/TodoHead.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import { useTodoState } from "../TodoContext";
 
 const TodoHeadBlock = styled.div`
   padding: 48px 32px 24px 32px;
@@ -24,11 +25,24 @@ const TodoHeadBlock = styled.div`
 `;
 
 function TodoHead() {
+  const todos = useTodoState();
+  const undoneTasks = todos.filter((todo) => !todo.done);
+
+  const today = new Date();
+  const dateString = today.toLocaleDateString("ko-KR", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+  const dayName = today.toLocaleDateString("ko-KR", {
+    weekday: "long",
+  });
+
   return (
     <TodoHeadBlock>
-      <h1>2020년 4월 26일</h1>
-      <p className="day">일요일</p>
-      <p className="tasks-left">할 일 2개 남음</p>
+      <h1>{dateString}</h1>
+      <p className="day">{dayName}</p>
+      <p className="tasks-left">할 일 {undoneTasks.length}개 남음</p>
     </TodoHeadBlock>
   );
 }
